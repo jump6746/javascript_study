a031160fdba742a594462dd6cf36858d759a9862
Jongmyeong Kim <102586637+jump6746@users.noreply.github.com>
2023-12-05 22:10:31 +0900

    [level 1] Title: 달리기 경주, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -10,29 +10,29 @@
 
 */
 
+// playersList = { mumu: 0, soe: 1, poe: 2, kai: 3, mine: 4}
 
-function solution(players, callings) {
-    const playerMap = {};
-  
-    // calling 원소들의 idx를 미리 구해서 저장
-    for (let i = 0; i < players.length; i++) {
-        playerMap[players[i]] = i;
-    }
-
-    for (let i = 0; i < callings.length; i++) {
-        const idx = playerMap[callings[i]];
-        const temp = players[idx-1];
-      
-        // 해당 idx와 이전 idx의 원소를 swap
-        players[idx-1] = callings[i];
-        players[idx] = temp;
-      
-        // map의 idx도 갱신
-        playerMap[callings[i]] = idx - 1;
-        playerMap[temp] = idx;
-    }
+function solution(players, callings){
+    let playersList = {};
     
-    return players;
+    for(let i = 0; i < players.length; i++){
+      playersList[players[i]] = i;
+    }
+  
+    for(let call of callings){
+      const index = playersList[call];
+      const temp = players[index - 1];
+
+      players[index - 1] = call;
+      players[index] = temp;
+
+      playersList[call] = index - 1;
+      playersList[temp] = index;
+    }
+
+    return players
 }
+
+
 
 
